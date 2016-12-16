699786078d1fdc6345f83dd44858af24e161b0b7
Jonas Jämtberg <jonas@jamtberg.se>
2016-12-16 13:47:44 +0100

    improved path handling

--- XsdToClassT4/TemplateDev.cs
+++ XsdToClassT4/TemplateDev.cs
@@ -8,30 +8,37 @@ using System.Threading.Tasks;
 
 namespace XsdToClassT4
 {
+    public class Host
+    {
+        public string ResolvePath(string asdf) { return null; }
+    }
+
     public class TemplateDev
     {
-        public static string Transform()
+        public string Transform()
         {
             // change this to your schema path!
             string[] schemaPaths = new[] {
                 @"C:\temp\T4Sc hemaToClass\T4SchemaToClass\Test\INT037.Movex.FuelTransactions.xsd"
             };
 
-
-
-
-
             var xsdPath = @"C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\xsd.exe";
+
+
+
+
+
+
 
             var runId = Guid.NewGuid();
             var workFolder = Path.Combine(Path.GetTempPath(), runId.ToString());
             Directory.CreateDirectory(workFolder);
 
             var schemaPathsString = schemaPaths
-                .Select(x => "\"" + x + "\"")
+                .Select(x => "\"" + Path.GetFullPath(this.Host.ResolvePath(x)) + "\"")
                 .Aggregate((a, n) => a + " " + n);
 
-            var process = Process.Start(new ProcessStartInfo
+            var startInfo = new ProcessStartInfo
             {
                 Arguments = string.Format("{0} /c /out:\"{1}\"", schemaPathsString, workFolder),
                 CreateNoWindow = true,
@@ -40,18 +47,22 @@ namespace XsdToClassT4
                 RedirectStandardInput = true,
                 RedirectStandardOutput = true,
                 UseShellExecute = false,
-            });
-            process.WaitForExit();
+            };
 
-            if (process.ExitCode != 0)
+            using (var process = Process.Start(startInfo))
             {
-                return "WorkFolder: " + workFolder + "\r\n\r\n" + process.StandardError.ReadToEnd();
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                {
+                    return "WorkFolder: " + workFolder + "\r\n\r\n" + process.StandardError.ReadToEnd();
+                }
             }
 
             var first = true;
-            string header;
-            string content;
-            foreach(var outFile in Directory.EnumerateFiles(workFolder))
+            var header = "";
+            var content = "";
+            foreach (var outFile in Directory.EnumerateFiles(workFolder))
             {
                 var lines = File.ReadAllLines(outFile);
 
@@ -72,6 +83,8 @@ namespace XsdToClassT4
 
             return null;
         }
-        private static void WriteLine(string sdfjklf) { }
+        private void WriteLine(string sdfjklf) { }
+
+        private Host Host { get; }
     }
 }
---
+++ XsdToClassT4/XsdToClass.cs
@@ -0,0 +1,3 @@
+﻿WorkFolder: C:\Users\Administrator\AppData\Local\Temp\704c3b8d-a847-4342-97f5-19bee48064c4
+
+Error: Could not find file "c:\temp\t4schematoclass\t4schematoclass\test\int037.movex.fueltransactions.xsd".
